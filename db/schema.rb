e667dc598213ca3d3344e80cfeeae7c27c001ccc
EC2 Default User <ec2-user@ip-172-31-39-12.ap-northeast-1.compute.internal>
2019-08-31 14:13:08 +0000

    add favorite column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190825144912) do
+ActiveRecord::Schema.define(version: 20190831140749) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20190825144912) do
     t.datetime "updated_at", null: false
     t.string "picture"
     t.integer "in_reply_to"
+    t.integer "favorite"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
