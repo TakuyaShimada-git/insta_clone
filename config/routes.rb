c576173f05d574cbfdfba8f8c3d7d430242f56ac
EC2 Default User <ec2-user@ip-172-31-39-12.ap-northeast-1.compute.internal>
2019-08-31 17:47:12 +0000

    Favoritesコントローラを作成。ルートも作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
@@ -16,4 +17,5 @@ Rails.application.routes.draw do
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
   resources :relationships,       only: [:create, :destroy]
+  resources :favorites,           only: [:create, :destroy]
 end
