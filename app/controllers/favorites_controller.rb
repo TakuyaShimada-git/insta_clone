edba32812d4ba2ca16ea93d06a1f5a92daa37321
EC2 Default User <ec2-user@ip-172-31-39-12.ap-northeast-1.compute.internal>
2019-09-01 10:18:21 +0000

    一応Ajax化してみた

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,10 +4,18 @@ class FavoritesController < ApplicationController
   def create
     @micropost = Micropost.find(params[:micropost_id])
     @micropost.favo(current_user)
+    respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+    end
   end
 
   def destroy
     @micropost = Favorite.find(params[:id]).micropost
     @micropost.unfavo(current_user)
+    respond_to do |format|
+        format.html { redirect_to request.referrer || root_url }
+        format.js
+    end
   end
 end
