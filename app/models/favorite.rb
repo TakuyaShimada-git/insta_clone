9db78de51eb2bf33b3d7b9069f0b07ab0e7ffaf0
EC2 Default User <ec2-user@ip-172-31-39-12.ap-northeast-1.compute.internal>
2019-09-01 08:49:53 +0000

    migrateを修正

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,5 +2,5 @@ class Favorite < ApplicationRecord
     belongs_to :user
     belongs_to :micropost
     validates :user_id, presence: true
-    validates :micropost_id, presence: true
+    validates :microposts_id, presence: true
 end
